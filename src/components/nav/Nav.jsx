42b877327a46b2b2ec7acc4a6a38acbefce64e9c
yhupaul <yhupaul@hotmail.com>
2022-06-08 23:44:24 -0700

    processing on nav bar styling

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -11,7 +11,7 @@ const Nav = () => {
     <nav>
       <a href='#home'><AiOutlineHome /></a>
       <a href='#about'><AiOutlineUser /></a>
-      <a href='#experiece'><BiBook /></a>
+      <a href='#experience'><BiBook /></a>
       <a href='#services'><RiServiceLine /></a>
       <a href='#contact'><BiMessageSquareDetail /></a>
     </nav>
