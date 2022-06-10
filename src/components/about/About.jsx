c8a84e705dbd6d31a6103bd653610a8ccf6edb90
yhupaul <yhupaul@hotmail.com>
2022-06-09 23:52:10 -0700

    styling about

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 import './about.css'
+import ME from '../../assets/ME.jpeg'
 
 const About = () => {
   return (
-    <section id="about">About</section>
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={ME} alt="About Image" />
+          </div>
+
+        </div>
+
+      </div>
+    </section>
   )
 }
 
