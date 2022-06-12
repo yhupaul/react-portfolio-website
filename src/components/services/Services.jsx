d4b8a621c9550d6498130716ff74af64558bf3e4
yhupaul <yhupaul@hotmail.com>
2022-06-11 22:13:24 -0700

    finished Services container and css

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,9 +1,118 @@
 import React from 'react'
 import './services.css'
+import {BiCheck} from 'react-icons/bi'
 
 const Services = () => {
   return (
-    <section id='services'>Services</section>
+    <section id='services'>
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+
+      <div className='container services__container'>
+        {/* UI/UX Design */}
+        <article className='service'>
+          <div className='service__head'>
+            <h3>UI/UX Design</h3>
+          </div>
+          <ul className='service__list'>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </p>
+            </li> 
+          </ul>
+        </article>
+        {/* END UI/UX Design */}
+        {/* Web Development */}
+        <article className='service'>
+          <div className='service__head'>
+            <h3>Web Development</h3>
+          </div>
+          <ul className='service__list'>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </p>
+            </li> 
+          </ul>
+        </article>
+        {/* END Web Development */}
+        {/* Content Creation */}
+        <article className='service'>
+          <div className='service__head'>
+            <h3>Content Creation</h3>
+          </div>
+          <ul className='service__list'>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              </p>
+            </li> 
+          </ul>
+        </article>
+        {/* END Content Creation */}
+      </div>
+    </section>
   )
 }
 
