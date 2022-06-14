89fd85ce2b5ad1a0efb85eca9269d827312b8303
yhupaul <yhupaul@hotmail.com>
2022-06-13 23:37:49 -0700

    stared portfolio comportent

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -3,7 +3,19 @@ import './portfolio.css'
 
 const Portfolio = () => {
   return (
-    <section id='portfolio'>Portfolio</section>
+    <section id='portfolio'>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
+
+      <div className='container portfolio__container'>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'></div>
+          <h3>This is a portfolio item title</h3>
+          <a href='https://github.com' className=''>Github</a>
+          <a href='https://github.com'>Github</a>
+        </article>
+      </div>
+    </section>
   )
 }
 
