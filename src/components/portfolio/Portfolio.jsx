bbe01e1ac8c2744852b9ef435a9f9cf7e3ebf24b
yhupaul <yhupaul@hotmail.com>
2022-06-14 11:12:52 -0700

    finished sytling for portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './portfolio.css'
+import IMG1 from '../../assets/momentem.png'
+import IMG2 from '../../assets/ydanmovie.png'
 
 const Portfolio = () => {
   return (
@@ -9,10 +11,24 @@ const Portfolio = () => {
 
       <div className='container portfolio__container'>
         <article className='portfolio__item'>
-          <div className='portfolio__item-image'></div>
-          <h3>This is a portfolio item title</h3>
-          <a href='https://github.com' className=''>Github</a>
-          <a href='https://github.com'>Github</a>
+          <div className='portfolio__item-image'>
+            <img src={IMG1} alt="" />
+          </div>
+          <h3>Momentem</h3>
+          <div className='portfolio__item-cta'>
+          <a href='https://github.com/yhupaul/momentum' className='btn' target='_blank'>Github</a>
+          <a href='https://yhupaul.github.io/momentum/' className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
+        </article>
+        <article className='portfolio__item'>
+          <div className='portfolio__item-image'>
+            <img src={IMG2} alt="" />
+          </div>
+          <h3>Ydanmovie</h3>
+          <div className="portfolio__item-cta">
+          <a href='https://github.com/yhupaul/YdanMovie' className='btn' target='_blank'>Github</a>
+          <a href='https://yhupaul.github.io/ydanmovie/' className='btn btn-primary' target='_blank'>Live Demo</a>
+          </div>
         </article>
       </div>
     </section>
