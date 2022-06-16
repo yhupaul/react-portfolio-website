5b52a2d19a7e32383537990e34305b34af680cb0
yhupaul <yhupaul@hotmail.com>
2022-06-15 23:34:37 -0700

    made footer.jsx file

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 import './footer.css'
+import {FaFacebookSquare} from 'react-icons/fa'
+import {RiInstagramFill} from 'react-icons/ri'
+import {FaTwitterSquare} from 'react-icons/fa'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href='"#' className='footer__logo'>Dan</a>
+
+      <ul className='permalinks'>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#about'>About</a></li>
+        <li><a href='#experience'>Experience</a></li>
+        <li><a href='#services'>Services</a></li>
+        <li><a href='#portfolio'>Portfolio</a></li>
+        <li><a href='#testimonials'>Testimonials</a></li>
+        <li><a href='#contact'>Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href='https://facebook.com'><FaFacebookSquare/></a>
+        <a href='https://instagram.com'><RiInstagramFill/></a>
+        <a href='https://twitter.com'><FaTwitterSquare/></a>
+      </div>
+      <div className='footer__copyright'>
+        <small>&copy; Dan's Portfolio. Everything is gonna be all right!!</small>
+      </div>
+    </footer>
   )
 }
 
