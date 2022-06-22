400f32ebceea5ed83014f4657d73e96ee00e6eea
yhupaul <yhupaul@hotmail.com>
2022-06-22 16:54:19 -0700

    remove taget

--- src/components/header/RTA.jsx
+++ src/components/header/RTA.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 const RTA = () => {
   return (
     <div className='rta'>
-      <a href="https://www.canva.com/design/DAE2zpbf2pY/CLodu_G9HcgO1lgn-b-_Pw/view?utm_content=DAE2zpbf2pY&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink" target="_blank" className='btn'>Resume</a>
+      <a href="https://www.canva.com/design/DAE2zpbf2pY/CLodu_G9HcgO1lgn-b-_Pw/view?utm_content=DAE2zpbf2pY&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink" className='btn'>Resume</a>
       <a href='#contact' className='btn btn-primary'>Let's Talk</a>
     </div>
   )
