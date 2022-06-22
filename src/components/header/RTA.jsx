1437d0c3ccf4822b39afc5c7868cca3b9c02356e
yhupaul <yhupaul@hotmail.com>
2022-06-22 15:58:05 -0700

    change download resume to link to canvar so that I only need to change on canva

--- src/components/header/RTA.jsx
+++ src/components/header/RTA.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-import RESUME from '../../assets/Dan you (3).pdf'
+
+// import RESUME from '../../assets/Dan you (3).pdf'
 
 const RTA = () => {
   return (
     <div className='rta'>
-      <a href={RESUME} download className='btn'>Download RS</a>
+      <a href="https://www.canva.com/design/DAE2zpbf2pY/CLodu_G9HcgO1lgn-b-_Pw/view?utm_content=DAE2zpbf2pY&utm_campaign=designshare&utm_medium=link&utm_source=publishsharelink" target="_blank" className='btn'>Resume</a>
       <a href='#contact' className='btn btn-primary'>Let's Talk</a>
     </div>
   )
