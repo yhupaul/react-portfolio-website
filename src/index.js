cf66981ceb5f9a9605cd33a8f3f00f641638f512
yhupaul <yhupaul@hotmail.com>
2022-06-22 23:54:46 -0700

    added hashrouter on indexjs

--- src/index.js
+++ src/index.js
@@ -4,7 +4,13 @@ import App from './App';
 import './index.css';
 
 import { createRoot } from 'react-dom/client';
+import { HashRouter } from 'react-router-dom';
 const container = document.querySelector("#root");
 const root = createRoot(container); 
-root.render(<App tab="home" />);
+root.render(
+  <HashRouter>
+  <App tab="home" />
+  </HashRouter>
+  );
+// root.render(<App tab="home" />);
 // ReactDOM.render(<App />, document.querySelector("#root"))
