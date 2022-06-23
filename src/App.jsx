52114bc119d0151cfa55219e04747d4fa75dc8fc
yhupaul <yhupaul@hotmail.com>
2022-06-23 11:15:28 -0700

    try again

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { HashRouter } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
 import About from './components/about/About'
@@ -12,17 +12,19 @@ import Footer from './components/footer/Footer'
 
 const App = () => {
   return (
+    <div>
     <HashRouter basename={process.env.PUBLIC_URL}>
-        <Header />
-        <Nav />
-        <About />
-        <Experience />
-        <Services />
-        <Portfolio />
-        <Testimonials />
-        <Contact />
-        <Footer />
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
     </HashRouter>   
+    </div>
   )
 }
 
