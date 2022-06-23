a07df42beb60e8e496ebc19984e016da322a0f49
yhupaul <yhupaul@hotmail.com>
2022-06-23 00:35:55 -0700

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,5 @@
 import React from 'react'
-// import { HashRouter } from 'react-router-dom'
-import {
-  HashRouter,
-  Routes,
-  Route,
-} from "react-router-dom"
+import { HashRouter } from 'react-router-dom'
 import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
 import About from './components/about/About'
@@ -18,20 +13,7 @@ import Footer from './components/footer/Footer'
 const App = () => {
   return (
     <div>
-      <HashRouter basename={process.env.PUBLIC_URL}>
-        <Routes>
-          <Route path='./react-portfolio-website' exact component={Header}/>
-          <Route path='./react-portfolio-website' exact component={Nav}/>
-          <Route path='./react-portfolio-website' exact component={About}/>
-          <Route path='./react-portfolio-website' exact component={Experience}/>
-          <Route path='./react-portfolio-website' exact component={Services}/>
-          <Route path='./react-portfolio-website' exact component={Portfolio}/>
-          <Route path='./react-portfolio-website' exact component={Testimonials}/>
-          <Route path='./react-portfolio-website' exact component={Contact}/>
-          <Route path='./react-portfolio-website' exact component={Footer}/>
-        </Routes>
-      </HashRouter>
-    {/* <HashRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter>
         <Header />
         <Nav />
         <About />
@@ -41,7 +23,7 @@ const App = () => {
         <Testimonials />
         <Contact />
         <Footer />
-    </HashRouter>    */}
+    </HashRouter>   
     </div>
   )
 }
