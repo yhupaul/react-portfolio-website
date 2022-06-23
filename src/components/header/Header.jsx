e781adf374d71d86b46248f6e0a7be51afe2cf50
yhupaul <yhupaul@hotmail.com>
2022-06-22 23:45:30 -0700

    change the img name

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css'
 import RTA from './RTA'
-import ME from '../../assets/mainIMG.jpg'
+import ME from '../../assets/main.jpg'
 import HeaderSocials from './HeaderSocials'
 
 const Header = () => {
